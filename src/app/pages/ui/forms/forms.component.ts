45a1975cc3745f55db62b311eafecfcd1f8d2d71
Vladislava <vladka_f@mail.ru>
2019-02-05 17:40:44 +0300

    feat(scroll): styling scroll
    feat(getmdl-select): stylization
    fix(dropdown): profile info cuts dropdown #7

--- src/app/pages/ui/forms/forms.component.ts
+++ src/app/pages/ui/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { AfterViewInit, Component, HostBinding } from '@angular/core';
 
 import { UpgradableComponent } from 'theme/components/upgradable';
 
@@ -7,11 +7,16 @@ import { UpgradableComponent } from 'theme/components/upgradable';
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss'],
 })
-export class FormsComponent extends UpgradableComponent {
+export class FormsComponent extends UpgradableComponent implements AfterViewInit {
   @HostBinding('class.mdl-grid') private readonly mdlGrid = true;
   @HostBinding('class.mdl-cell') private readonly mdlCell = true;
   @HostBinding('class.mdl-cell--12-col-desktop') private readonly mdlCell12ColDesktop = true;
   @HostBinding('class.mdl-cell--12-col-tablet') private readonly mdlCell12ColTablet = true;
   @HostBinding('class.mdl-cell--4-col-phone') private readonly mdlCell4ColPhone = true;
   @HostBinding('class.mdl-cell--top') private readonly mdlCellTop = true;
+
+  public ngAfterViewInit() {
+    getmdlSelect.init('.getmdl-select');
+    componentHandler.upgradeDom();
+  }
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,7 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+declare var getmdlSelect: {
+  init: any;
+};
