3ee9de850c93be70ec50feba5b8ed4f38f53afd1
Vladimir Sernatsky <byport1112@gmail.com>
2019-06-04 17:16:45 +0300

    build(angular): update angular to 8

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AuthService } from '@services/';
+import { AuthService } from '@services/*';
 import { BlankLayoutCardComponent } from 'app/components/blank-layout-card';
 
 @Component({
